df1e805a141673bda4aeafd0e8e00e1f57e72714
aribhatt <aribhatt@adobe.com>
2017-09-11 18:48:19 +0530

    Changes to home page profile css

--- src/pages/summary-detail/summary-detail.ts
+++ src/pages/summary-detail/summary-detail.ts
@@ -16,6 +16,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class SummaryDetailPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    console.log('inside detail', navParams);
   }
 
   ionViewDidLoad() {
